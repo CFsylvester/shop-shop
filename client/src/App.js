dd56604a777a4d7c24e408552c54e8b03b0a434a
Claire Sylvester <clairesylvstr@gmail.com>
2021-01-23 15:02:08 -0500

    stripe working

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
+import {StoreProvider} from './utils/GlobalState'; 
 
+import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Nav from "./components/Nav";
-import {StoreProvider} from './utils/GlobalState'; 
+import Success from './pages/Success';
 import OrderHistory from "./pages/OrderHistory";
 
 const client = new ApolloClient({
@@ -37,6 +38,7 @@ function App() {
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} />
+            <Route exact path="/success" component={Success} />
             <Route component={NoMatch} />
           </Switch>
           </StoreProvider>
